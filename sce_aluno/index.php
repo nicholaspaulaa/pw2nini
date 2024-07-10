b15a0d255346fa5676849cf7832a9057ce14a9f0
nicholaspaulaa <nicholas.emanuell1@gmail.com>
2024-07-10 14:19:41 -0300

    corno

---
+++ sce_aluno/index.php
@@ -0,0 +1,9 @@
+<?php
+    include('cabecalho.php');
+?>
+    <div class="container">
+        <h3>Bem vindo ao Sistema de Controle de Estoque</h3>
+    </div>
+<?php
+    include('rodape.php');
+?>
